dc9421deb9bcd709fb16d5b4c69286ef2bcc8a96
Natalie Mosqueda <natalie_bustos18@Natalies-MacBook-Pro-5.local>
2018-07-25 18:34:11 -0500

    Added #1 of Java loops

---
+++ src/ControlFlowExercises.java
@@ -0,0 +1,24 @@
+public class ControlFlowExercises {
+    public static void main(String[] args) {
+//        1.A
+//        int i = 5;
+//        while (i <= 15) {
+//            System.out.println(i);
+//            i++;
+//        }
+//        for(int i = 5; i <= 15; i++){
+//            System.out.println(i);
+//        }
+//        1.B
+//        long x = 2;
+//        do {
+//            System.out.println(x);
+//            x *= x;
+//        }while (x < 1000000);
+//         1.C
+//        for(long x = 2; x < 1000000; x *= x){
+//            System.out.println(x);
+//        }
+
+    }
+}
