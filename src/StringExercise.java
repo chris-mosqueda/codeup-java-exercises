c94d7256437f98e3d34fb0a37b26213118794cd3
Natalie Mosqueda <natalie_bustos18@Natalies-MacBook-Pro-5.local>
2018-07-26 10:03:58 -0500

    Completed String exercise, finish Bob.java

---
+++ src/StringExercise.java
@@ -0,0 +1,15 @@
+public class StringExercise {
+    public static void main(String[] args) {
+        String word = "We don't need no education\nWe don't need no thought control\n";
+
+        word += "\nCheck \"this\" out!, \"s inside of \"s!\n";
+
+        word += "\nIn windows, the main drive is usually in C:\\";
+
+        word += "I can do backslashes \\, double backslashes \\\\,\n" +
+                "and the amazing triple backslash \\\\\\!";
+
+        System.out.println(word);
+
+    }
+}
