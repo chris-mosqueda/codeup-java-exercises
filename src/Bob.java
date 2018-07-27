521d1d201d2e1cf9f24dee606c0d1c791d097772
Natalie Mosqueda <natalie_bustos18@Natalies-MacBook-Pro-5.local>
2018-07-27 13:57:59 -0500

    Completed Bob.java exercise

--- src/Bob.java
+++ src/Bob.java
@@ -1,5 +1,23 @@
+import java.util.Scanner;
+
 public class Bob {
     public static void main(String[] args) {
-
+        boolean endConvo = true;
+        while(endConvo){
+            Scanner sc = new Scanner(System.in);
+            System.out.print("Let's talk to Bob:\nTo stop conversation type \"Stop\": ");
+            String convo = sc.nextLine();
+            if(convo.endsWith("!")){
+                System.out.println("\nBob: Whoa, chill out!\n");
+            } else if(convo.endsWith("?")){
+                System.out.println("\nBob: Sure\n");
+            } else if(convo.equals("")){
+                System.out.println("\nBob: Fine. Be that way!\n");
+            } else if(convo.equalsIgnoreCase("stop")){
+                endConvo = false;
+            } else {
+                System.out.println("\nBob: Whatever\n");
+            }
+        }
     }
 }
